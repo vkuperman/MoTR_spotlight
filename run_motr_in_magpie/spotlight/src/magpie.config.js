f5fc33b0dcabdefc5b33b447bf9a254a4236e257
vkuperman <vkuperman@yahoo.com>
2026-05-27 14:28:01 -0400

    Update resultsUploadUrl in magpie.config.js

--- run_motr_in_magpie/spotlight/src/magpie.config.js
+++ run_motr_in_magpie/spotlight/src/magpie.config.js
@@ -6,5 +6,5 @@ export default {
   contactEmail: 'cui.ding@uzh.ch',
   mode: 'debug',
   language: 'en',
-  resultsUploadUrl: 'https://mo-tr-click.vercel.app/api/upload-results'
+  resultsUploadUrl: 'https://mo-tr-spotlight.vercel.app/api/upload-results'
 };
